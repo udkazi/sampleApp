f5257b7e26c2542bfb08075b653c8430a7c8763e
ubed kazi <udkazi777@gmail.com>
2016-03-24 16:14:41 +0530

    update

---
+++ models/index.js
@@ -0,0 +1,4 @@
+module.exports = {
+  movie: require('./Movie'),
+  movie: require('./new')
+};
---
+++ routes.js
@@ -0,0 +1,5 @@
+module.exports = {
+  'movie': require('./controllers/MovieController'),
+  'new': require('./controllers/newMovieController')
+
+};
